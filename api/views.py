2f7152a776de8fc157ac4fe32c9b10fc5ac19698
wafulahvictor@gmail.com <wafulahvictor@gmail.com>
2024-06-21 01:45:15 +0300

    user

--- api/views.py
+++ api/views.py
@@ -177,7 +177,7 @@ def get_user_by_id(request):
             
             if (access_token):
                 request.session['session_token'] = access_token 
-                request.session['user'] = user
+                request.session['user-id'] = user.id
             
             session_token = request.session.get('session_token')
 
