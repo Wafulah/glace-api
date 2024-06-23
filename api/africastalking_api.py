23a6d2aa2213e36faf1eba8a708941161b601e60
wafulahvictor@gmail.com <wafulahvictor@gmail.com>
2024-06-24 00:02:05 +0300

    recipient

--- api/africastalking_api.py
+++ api/africastalking_api.py
@@ -8,20 +8,20 @@ logger = logging.getLogger(__name__)
 AFRICASTALKING_USERNAME = "sandbox"
 AFRICASTALKING_API_KEY = os.getenv("AT_API_KEY")
 AFRICASTALKING_ENDPOINT = "https://api.sandbox.africastalking.com/version1/messaging"
-
 def send_sms(recipient, message):
 
     headers = {
-        "Authorization": f"Bearer {AFRICASTALKING_API_KEY}",
-        "Content-Type": "application/json",
+        "apiKey": AFRICASTALKING_API_KEY,
+        "Content-Type": "application/x-www-form-urlencoded",
     }
 
     data = {
+        "username": AFRICASTALKING_USERNAME,
         "to": recipient,
-        "from": "70142",  
+        "from": "70142",
         "message": message,
     }
 
-    response = requests.post(AFRICASTALKING_ENDPOINT, headers=headers, json=data)
+    response = requests.post(AFRICASTALKING_ENDPOINT, headers=headers, data=data)
     logger.error("[AFT_POST_RESPONSE] %s", response)
     return response.json()
